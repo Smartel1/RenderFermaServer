500a376454fa90877d136b8cd42cf99884af7eb0
drews <as@bstd.ru>
2019-10-22 13:53:17 +0500

    user duplicates

--- src/main/java/ru/smartel/renderferma/server/controller/UserController.java
+++ src/main/java/ru/smartel/renderferma/server/controller/UserController.java
@@ -1,6 +1,8 @@
 package ru.smartel.renderferma.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,12 +27,18 @@ public class UserController {
      * User registration endpoint
      */
     @PostMapping
-    public void register(@RequestBody @Validated RegistrationDTO userRequestDTO) {
-        UserEntity user = new UserEntity();
+    public ResponseEntity<String> register(@RequestBody @Validated RegistrationDTO userRequestDTO) {
+        UserEntity user = userRepository.findByEmail(userRequestDTO.getEmail());
+        if (null != user) {
+            return new ResponseEntity<>("User with this email already exists", null, HttpStatus.CONFLICT);
+        }
+
+        user = new UserEntity();
         user.setEmail(userRequestDTO.getEmail());
         user.setPasswordHash(
                 passwordEncoder.encode(userRequestDTO.getPassword())
         );
         userRepository.save(user);
+        return new ResponseEntity<>("User created", null, HttpStatus.OK);
     }
 }
